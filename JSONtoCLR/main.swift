95089d267edd31b83d655bce6530ad599665a80c
Christoph Labacher <christoph.labacher@gmail.com>
2018-06-08 15:50:28 +0200

    Initial Commit

---
+++ JSONtoCLR/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  JSONtoCLR
+//
+//  Created by Christoph Labacher on 08.06.18.
+//  Copyright © 2018 Christoph Labacher. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
